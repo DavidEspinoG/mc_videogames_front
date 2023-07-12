be850be52ddf3e06c525da69f4869ae4f52115da
Dico Diaz <dicodiaz@gmail.com>
2023-07-12 17:46:26 -0500

    Set up relevant dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <>
-      <h1>Hello Vite + React!</h1>
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<h1>Hello world</h1>} />
+    </Routes>
+  </Router>
+);
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.css';
 import store from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
