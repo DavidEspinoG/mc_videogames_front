ff53a1528b47fe68507a5a2d27ebe08d144c0505
David Espino <davidespinog@gmail.com>
2023-07-12 16:02:47 -0600

    Install and configure redux

---
+++ src/redux/slices/userSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    name: '',
+    jwt: '',
+    isAdmin: false
+  }
+});
+
+export default userSlice.reducer;
