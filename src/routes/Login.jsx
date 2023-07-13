23334ed2df41b1f74ab06e5d4462036cb3eeab04
David Espino <davidespinog@gmail.com>
2023-07-13 13:51:01 -0600

    Fix linters

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,27 +1,25 @@
 import '../styles/Login.scss';
 
-const Login = () => {
-  return (
-    <div className="login-main-container">
-      <div className='container'>
-        <div className='login-header'>
-          <i class="fa-solid fa-bars login-icon-one"></i>
-          <i class="fa-solid fa-magnifying-glass login-icon-two"></i>
-        </div>
-        <div className='login-form'>
-          <h3 className='subtitle text-center'>LOGIN</h3>
-          <hr className='green-line'/>
-          <div className='login-inputs'>
-            <input placeholder='Email' className='input'/>
-            <input placeholder='Password' className='input' />
-            <button type='button' className='login-submit-button'>
-              Submit
-            </button>
-          </div>
+const Login = () => (
+  <div className="login-main-container">
+    <div className="container">
+      <div className="login-header">
+        <i className="fa-solid fa-bars login-icon-one" />
+        <i className="fa-solid fa-magnifying-glass login-icon-two" />
+      </div>
+      <div className="login-form">
+        <h3 className="subtitle text-center">LOGIN</h3>
+        <hr className="green-line" />
+        <div className="login-inputs">
+          <input placeholder="Email" className="input" />
+          <input placeholder="Password" className="input" />
+          <button type="button" className="login-submit-button">
+            Submit
+          </button>
         </div>
       </div>
     </div>
-  )
-};
+  </div>
+);
 
 export default Login;
