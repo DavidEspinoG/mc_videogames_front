f342d6872151de21dbcf4fbfe9b4e665df094ce0
David Espino <davidespinog@gmail.com>
2023-07-13 13:49:47 -0600

    Add styles to login form

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -8,8 +8,18 @@ const Login = () => {
           <i class="fa-solid fa-bars login-icon-one"></i>
           <i class="fa-solid fa-magnifying-glass login-icon-two"></i>
         </div>
+        <div className='login-form'>
+          <h3 className='subtitle text-center'>LOGIN</h3>
+          <hr className='green-line'/>
+          <div className='login-inputs'>
+            <input placeholder='Email' className='input'/>
+            <input placeholder='Password' className='input' />
+            <button type='button' className='login-submit-button'>
+              Submit
+            </button>
+          </div>
+        </div>
       </div>
-     
     </div>
   )
 };
