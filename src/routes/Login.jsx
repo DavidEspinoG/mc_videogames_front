dec757de9a389bbc388dde4b05f7fb322fac2cb6
David Espino <davidespinog@gmail.com>
2023-07-13 13:13:45 -0600

    Add background image to login view

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,5 +1,17 @@
-const Login = () => (
-  <h1>Login</h1>
-);
+import '../styles/Login.scss';
+
+const Login = () => {
+  return (
+    <div className="login-main-container">
+      <div className='container'>
+        <div className='login-header'>
+          <i class="fa-solid fa-bars login-icon-one"></i>
+          <i class="fa-solid fa-magnifying-glass login-icon-two"></i>
+        </div>
+      </div>
+     
+    </div>
+  )
+};
 
 export default Login;
