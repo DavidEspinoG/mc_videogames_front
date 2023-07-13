32127c5286bb91eb815c4c6ee8269e43d0c738be
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-13 18:03:10 -0500

    Wrap each route with a main tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navigation from './components/navigation';
 import AddVideogame from './routes/AddVideogame';
 import DeleteVideogame from './routes/DeleteVideogame';
 import Details from './routes/Details';
@@ -10,16 +11,19 @@ import Reserve from './routes/Reserve';
 
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/reserve" element={<Reserve />} />
-      <Route path="/details/:id" element={<Details />} />
-      <Route path="/myReservations" element={<MyReservations />} />
-      <Route path="/add" element={<AddVideogame />} />
-      <Route path="/delete" element={<DeleteVideogame />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Navigation />
+    <main className="main">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reserve" element={<Reserve />} />
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/myReservations" element={<MyReservations />} />
+        <Route path="/add" element={<AddVideogame />} />
+        <Route path="/delete" element={<DeleteVideogame />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
   </Router>
 );
 
