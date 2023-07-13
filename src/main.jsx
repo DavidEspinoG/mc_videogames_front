4bec7126cdee7c41264a7d6d763277d2beeda1cf
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-13 18:03:41 -0500

    Import styles to main.js

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
