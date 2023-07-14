1f313de9338af674d515a6c69f8b3484e75c1957
David Espino <davidespinog@gmail.com>
2023-07-14 14:49:49 -0600

    Remove unnecessary icons from login view

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,25 +1,29 @@
+import { BiArrowBack } from 'react-icons/bi';
 import '../styles/Login.scss';
+import { useNavigate } from 'react-router-dom';
 
-const Login = () => (
-  <div className="login-main-container">
-    <div className="container">
-      <div className="login-header">
-        <i className="fa-solid fa-bars login-icon-one" />
-        <i className="fa-solid fa-magnifying-glass login-icon-two" />
-      </div>
-      <div className="login-form">
-        <h3 className="subtitle text-center">LOGIN</h3>
-        <hr className="green-line" />
-        <div className="login-inputs">
-          <input placeholder="Email" className="input" />
-          <input placeholder="Password" className="input" />
-          <button type="button" className="login-submit-button">
-            Submit
-          </button>
+const Login = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="login-main-container">
+      <div className="container">
+        <div className="login-header">
+          <BiArrowBack className="back-icon" onClick={() => navigate('/')} />
+        </div>
+        <div className="login-form">
+          <h3 className="subtitle text-center">LOGIN</h3>
+          <hr className="green-line" />
+          <div className="login-inputs">
+            <input placeholder="Email" className="input" />
+            <input placeholder="Password" className="input" />
+            <button type="button" className="login-submit-button">
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Login;
