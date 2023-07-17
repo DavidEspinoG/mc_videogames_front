0892e239a3f87956a5ae3138b56c087c1b6559db
David Espino <davidespinog@gmail.com>
2023-07-17 09:41:37 -0600

    Merge from stash

--- src/routes/Reserve.jsx
+++ src/routes/Reserve.jsx
@@ -1,5 +1,58 @@
-const Reserve = () => (
-  <h1>Reserve</h1>
-);
+import { BiArrowBack } from 'react-icons/bi';
+import '../styles/Login.scss';
+import { useNavigate } from 'react-router-dom';
+import '../styles/reserve.css';
+
+const mockData = [
+  {
+    id: 1,
+    name: 'Zelda: Breath of the wild', 
+    photo: 'http://fakeurl.com',
+    description: 'A very interesting game', 
+    price_per_day: 20
+  },
+  {
+    id: 2,
+    name: 'Hogwarts Legacy', 
+    photo: 'http://fakeurl.com',
+    description: 'A very interesting game', 
+    price_per_day: 30
+  },
+  {
+    id: 3,
+    name: 'God of war', 
+    photo: 'http://fakeurl.com',
+    description: 'A very interesting game', 
+    price_per_day: 35
+  },
+];
+const Reserve = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="login-main-container">
+      <div className="container">
+        <div className="login-header">
+          <BiArrowBack className="back-icon" onClick={() => navigate('/')} />
+        </div>
+        <div className="login-form">
+          <h3 className="subtitle text-center">RESERVE A VIDEOGAME</h3>
+          <hr className="green-line" />
+          <div className="login-inputs">
+            <select className='reserve-select'>
+              {mockData.map(element => (
+                <option key={element.id} value={element.id}>{element.name}</option>))
+              }
+            </select>
+            <input placeholder="Username" value={'current user name'} disabled={true} className="input" />
+            <input type='date' className='input' />
+            <button type="button" className="login-submit-button">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default Reserve;
