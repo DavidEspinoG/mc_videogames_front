6e2ff991f0505ca0fbd407e29da6422a1f7a3d5c
David Espino <davidespinog@gmail.com>
2023-07-17 12:56:15 -0600

    Add days counter to reservation view

--- src/routes/Reserve.jsx
+++ src/routes/Reserve.jsx
@@ -1,22 +1,26 @@
 import { BiArrowBack } from 'react-icons/bi';
 import '../styles/Login.scss';
 import { useNavigate } from 'react-router-dom';
-import '../styles/reserve.css';
-import { useSearchParams } from 'react-router-dom';
+import '../styles/reserve.scss';
 import { useSelector } from 'react-redux';
 import getTomorrowDate from '../utils/getTomorrowDate';
+import { useState } from 'react';
 
 const Reserve = () => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
   const mockData = useSelector(state => state.videogames.all);
-  const handleChange = (e) => {
+  const [days, setDays] = useState(0);
+  const [selectedVideogameId, setSelectedVideogameId] = useState(0);
+  const handleDateChange = (e) => {
     let now = new Date();
     let userDate = new Date(e.target.value);
     let difference = userDate.getTime() - now.getTime();
     let differenceDays = Math.ceil(difference / (1000 * 3600 * 24));
-    console.log(differenceDays);
+    setDays(differenceDays);
   };
+  const handleVideogameChange = (e) => {
+    setSelectedVideogameId(e.target.value);
+  }
   return (
     <div className="login-main-container">
       <div className="container">
@@ -24,10 +28,16 @@ const Reserve = () => {
           <BiArrowBack className="back-icon" onClick={() => navigate('/')} />
         </div>
         <div className="login-form">
-          <h3 className="subtitle text-center">RESERVE A VIDEOGAME {searchParams.get('videogameId')} </h3>
+          <h3 className="subtitle text-center">RESERVE A VIDEOGAME </h3>
           <hr className="green-line" />
+          <p>Select the videogame you want to rent and the date you want to return it</p>
           <div className="login-inputs">
-            <select className="reserve-select">
+            <select 
+              className="reserve-select" 
+              value={selectedVideogameId} 
+              onChange={handleVideogameChange}
+            >
+              <option value={0}>-- Select a videogame --</option>
               {mockData.map((element) => (
                 <option key={element.id} value={element.id}>{element.name}</option>))}
             </select>
@@ -35,12 +45,21 @@ const Reserve = () => {
               type="date" 
               min={getTomorrowDate()}
               className="input" 
-              onChange={(e) => {handleChange(e)}}
+              onChange={(e) => {handleDateChange(e)}}
             />
-            <button type="button" className="login-submit-button">
+            <button 
+              type="button" 
+              className="login-submit-button"
+              onClick={() => {
+                console.log('days', days)
+                console.log('videogameId', selectedVideogameId)
+              }}
+            >
               Submit
             </button>
           </div>
+          {days ? <p>*Your rent will last {days} days</p> : ''}
+          
         </div>
       </div>
     </div>
