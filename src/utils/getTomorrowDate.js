e30c732183ff3da28dc94dfb3efd3979edf5d5ce
David Espino <davidespinog@gmail.com>
2023-07-18 09:14:10 -0600

    Fix date bug in reserve form

--- src/utils/getTomorrowDate.js
+++ src/utils/getTomorrowDate.js
@@ -1,5 +1,6 @@
 const getTomorrowDate = () => {
   const now = new Date();
+  now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
   const tomorrow = new Date(now);
   tomorrow.setDate(now.getDate() + 1);
   return tomorrow.toISOString().split('T')[0];
