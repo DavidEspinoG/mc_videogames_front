837dc0241dc75c1cc392f4e5dedba879606f4826
NicolasAndrehh <nicolasolaya22@gmail.com>
2023-07-21 16:16:14 -0500

    Make Details component DRY

--- src/routes/Details.jsx
+++ src/routes/Details.jsx
@@ -15,6 +15,12 @@ const Details = () => {
   const videogameDetails = useSelector(selectVideogameDetails);
   const error = useSelector(selectVideogameDetailsError);
 
+  const pricingTable = [
+    { text: 'Price per day', multiplier: 1 },
+    { text: 'Price per week', multiplier: 7 },
+    { text: 'Price per month', multiplier: 30 },
+  ];
+
   useEffect(() => {
     dispatch(getDetails(id));
   }, [dispatch, id]);
@@ -41,49 +47,35 @@ const Details = () => {
       <div className="details">
         <h2 className="title">{name}</h2>
         <p className="description">{description}</p>
+
         <table className="pricing-table">
           <tbody>
-            <tr>
-              <td>
-                Price per day:
-                {' '}
-                <span>
-                  $
-                  {pricePerDay}
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Price per week:
-                {' '}
-                <span>
-                  $
-                  {pricePerDay * 7}
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Price per month:
-                {' '}
-                <span>
-                  $
-                  {pricePerDay * 30}
-                </span>
-              </td>
-            </tr>
+            {pricingTable.map((item) => (
+              <tr key={item.text}>
+                <td>
+                  {item.text}
+                  {' '}
+                  <span>
+                    $
+                    {pricePerDay * item.multiplier}
+                  </span>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
+
         <Link to="/" className="discover-button">
           Discover more videogames
           {' '}
           <span>&gt;</span>
         </Link>
+
         <div className="image-container">
           <BiGame className="icon" />
           <img src={colorWheel} alt="color wheel" className="color-wheel" />
         </div>
+
         <Link to={`/reserve?videogameId=${id}`} className="reserve-button">
           Reserve
           <MdKeyboardArrowRight className="icon" />
