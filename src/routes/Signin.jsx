4c68e4f8e64a706d26dea776076d704fdbe9b77d
David Espino <davidespinog@gmail.com>
2023-07-21 22:04:30 -0600

    Fix small design issues on signin view

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import '../styles/Signin.scss';
+import { BiArrowBack } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 import { signin } from '../redux/slices/userSlice';
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -23,68 +27,74 @@ const SignIn = () => {
   };
 
   return (
-    <div className="login-main-container">
-      <div className="container">
-        <div className="login-form">
-          <h3 className="subtitle text-center">SIGN IN</h3>
-          <hr className="green-line" />
-          <form
-            onSubmit={handleSubmit}
-            className="login-inputs"
-          >
-            <input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Name"
-              className="input"
-              type="text"
-            />
-            <input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="E-mail"
-              className="input"
-              type="text"
-            />
-            <input
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              className="input"
-              type="password"
-            />
-            <input
-              value={passwordConfirmation}
-              onChange={(e) => setPasswordConfirmation(e.target.value)}
-              placeholder="Confirm your password"
-              className="input"
-              type="text"
-            />
-            <input
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              placeholder="Address"
-              className="input"
-              type="text"
-            />
-            <button
-              type="submit"
-              className="login-submit-button"
-            >
-              {signinLoading ? <div className="spinner-border" /> : 'Submit'}
-            </button>
-          </form>
-          {signinError && (
-          <small>
-            *
-            {' '}
-            {signinError}
-            {' '}
-            *
-          </small>
-          )}
-        </div>
+    <div className="signin-main-container">
+      <div className="signin-header">
+        <BiArrowBack className="back-icon" onClick={() => navigate('/')} />
       </div>
+      <h3 className="subtitle text-center">SIGN IN</h3>
+      <div className="line-center">
+        <hr className="green-line" />
+      </div>
+      <form
+        onSubmit={handleSubmit}
+      >
+        <div className="signin-inputs">
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name"
+            className="input"
+            type="text"
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail"
+            className="input"
+            type="text"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="input"
+            type="password"
+          />
+        </div>
+        <div className="signin-inputs">
+          <input
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+            placeholder="Confirm your password"
+            className="input"
+            type="password"
+          />
+          <input
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            placeholder="Address"
+            className="input"
+            type="text"
+          />
+        </div>
+        <div className="signin-inputs">
+          <button
+            type="submit"
+            className="login-submit-button"
+          >
+            {signinLoading ? <div className="spinner-border" /> : 'Submit'}
+          </button>
+        </div>
+      </form>
+      {signinError && (
+      <small>
+        *
+        {' '}
+        {signinError}
+        {' '}
+        *
+      </small>
+      )}
     </div>
   );
 };
